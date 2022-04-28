0c0cb72bfc8f0af7ba0b24af9082b3781fdf3333
NereySer <67207071+NereySer@users.noreply.github.com>
2022-04-28 16:10:11 +0300

    Create time_limits.py

---
+++ modules/time_limits.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timezone, timedelta
+
+def getStart():
+    return datetime.utcnow().isoformat() + 'Z'
+
+def getEnd(): 
+    next_day = datetime.now(timezone(timedelta(hours=+3))) + timedelta(days = 1)
+
+    end_next_day = next_day.replace(hour=23, minute=59)
+
+    return end_next_day.astimezone(timezone.utc).isoformat() + 'Z'
