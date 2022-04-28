d4917d86b63361cfe598647f51693b90f618588c
NereySer <67207071+NereySer@users.noreply.github.com>
2022-04-28 16:33:42 +0300

    Update time_limits.py

--- modules/time_limits.py
+++ modules/time_limits.py
@@ -6,6 +6,6 @@ def getStart():
 def getEnd(): 
     next_day = datetime.now(timezone(timedelta(hours=+3))) + timedelta(days = 1)
 
-    end_next_day = next_day.replace(hour=23, minute=59)
+    end_next_day = next_day.replace(hour=23, minute=59, second=59, microsecond=0)
 
     return end_next_day.astimezone(timezone.utc).isoformat() + 'Z'
