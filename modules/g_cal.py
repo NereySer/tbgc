df4780c3f4871a9057bbfb3dd2120570507cdc22
NereySer <67207071+NereySer@users.noreply.github.com>
2022-04-30 10:14:57 +0300

    Update g_cal.py

--- modules/g_cal.py
+++ modules/g_cal.py
@@ -17,7 +17,7 @@ def get_incomig_events(begin: str, end: str):
     
     retval += 'Getting the upcoming 10 events\n'
     events_result = g_service.events().list(calendarId=calendarId,
-                                            timeMin=t.getStart(), timeMax=t.getEnd(),
+                                            timeMin=begin, timeMax=end,
                                             singleEvents=True,
                                             orderBy='startTime').execute()
     events = events_result.get('items', [])
