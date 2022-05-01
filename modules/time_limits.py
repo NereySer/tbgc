1234a708e0d2fc418ab7f6257b5de6a6f23f4334
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-01 22:49:48 +0300

    Update time_limits.py

--- modules/time_limits.py
+++ modules/time_limits.py
@@ -17,19 +17,3 @@ def getTimeBounds():
         'end': end.astimezone(timezone.utc).replace(tzinfo=None).isoformat() + 'Z'    
     }
 
-"""
-def getStart():
-    return datetime.utcnow().isoformat() + 'Z'
-
-def getEnd(): 
-    now = datetime.now(DEFAULT_TIMEZONE)
-    
-    if now.hour > 12:
-        end_day = now + timedelta(days = 1)
-    else:
-        end_day = now
-    
-    end_day = end_day.replace(hour=23, minute=59, second=59, microsecond=0)
-
-    return end_day.astimezone(timezone.utc).replace(tzinfo=None).isoformat() + 'Z'
-"""
