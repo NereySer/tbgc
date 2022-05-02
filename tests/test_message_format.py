6cc576b05ed6bb3ccabcdba4fabd4b8e78de675a
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-02 15:29:52 +0300

    Update test_message_format.py

--- tests/test_message_format.py
+++ tests/test_message_format.py
@@ -1,12 +1,27 @@
 from modules import message_format
 
-def test_work():
-    assert message_format.format([
+@pytest.mark.parametrize("events, expected", [
+    ([{
+        'summary': 'test',
+        'start': {
+            'dateTime': '2022-05-03T19:00:00+03:00'
+        }
+    }], '2022-05-03T19:00:00+03:00 test\n'),
+    ([
         {
             'summary': 'test',
             'start': {
                 'dateTime': '2022-05-03T19:00:00+03:00'
             }
+        }, 
+        {
+            'summary': 'second_test',
+            'start': {
+                'dateTime': '2022-05-03T20:00:00+03:00'
+            }
         }
-    ]) == '2022-05-03T19:00:00+03:00 test\n'
+    ], '2022-05-03T19:00:00+03:00 test\n2022-05-03T20:00:00+03:00 second_test\n')
+])
+def test_work(events, expected):
+    assert message_format.format(events) == expected
     
