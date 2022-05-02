c504e53b3ec0c1d673bcf863e1b800227cbcd05f
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-02 17:25:34 +0300

    Update test_time_checks.py

--- tests/test_time_checks.py
+++ tests/test_time_checks.py
@@ -10,6 +10,43 @@ def getTime(time):
     
     return time
 
+def generate_event(hour, day_add):
+    return {
+        'start': {
+            'dateTime': (datetime.now(DEFAULT_TIMEZONE).replace(hour=hour) + timedelta(days = days_add)).isoformat()
+        }
+    }
+
+@pytest.mark.parametrize("set_hour, events, expected", [
+    #Morning reminders
+    (9, [generate_event(10, 0)], true),
+    (9, [generate_event(11, 0)], true),
+    (9, [generate_event(16, 0)], true),
+    (9, [generate_event(17, 0)], false),
+    #Daytime reminders
+    (11, [generate_event(17, 0)], true),
+    (12, [generate_event(17, 0)], true),
+    (12, [generate_event(23, 0)], true),
+    #Evening reminders
+    (12, [generate_event(9, 1)], false),
+    (13, [generate_event(9, 1)], true),
+    (13, [generate_event(11, 1)], true),
+    (13, [generate_event(12, 1)], false)
+])
+def test_isTimeToRemind_single_event(monkeypatch, set_hour, events, expected: bool):
+    class mock_datetime:
+        @classmethod
+        def now(self, tz=None):
+            return ( datetime.now(DEFAULT_TIMEZONE).replace(hour=set_hour).astimezone(tz) )
+        @classmethod
+        def utcnow(self):
+            return ( self.now(timezone.utc) )
+    
+    monkeypatch.setattr(time_checks, 'datetime', mock_datetime)
+    
+    assert modules.isTimeToRemind(events) == expected
+    
+
 @pytest.mark.parametrize("set_hour", [9, 10, 12, 13, 23])
 def test_time_bounds(monkeypatch, set_hour):
     days_diff = 1 if set_hour > LATE_HOUR else 0
