fdd3a0f425d559d96f0630bbd7a7b1b870cebb90
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-04 13:20:52 +0300

    Update message_format.py

--- modules/message_format.py
+++ modules/message_format.py
@@ -3,9 +3,10 @@ from jinja2 import Template
 template = None
 
 def initTemplate():
+    global template
     if template is None:
-        html = open('foopkg/templates/0.hello.html').read()
-        template = Template(html)
+        j2 = open('templates/telegram_message.j2').read()
+        template = Template(j2)
 
 def format(events) -> str:
     initTemplate()
