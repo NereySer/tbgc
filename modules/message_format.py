1c735fbf7423e0ffa9d9d8b13d600cbdc113cf1c
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-04 12:33:55 +0300

    Update message_format.py

--- modules/message_format.py
+++ modules/message_format.py
@@ -1,11 +1,3 @@
 
 def format(events) -> str:
-    if not events:
-        retval = 'No upcoming events found.\n'
-    else:
-        retval = ''
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            retval += start + ' ' + event['summary'] + '\n'
-        
-    return retval
+    return render_template('templates/telegram_message', events=events)
