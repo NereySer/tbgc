07de603f0a0586df23f9bb20bb341d4cea11bda2
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-05 16:13:33 +0300

    Update g_cal.py

--- modules/g_cal.py
+++ modules/g_cal.py
@@ -16,10 +16,6 @@ SERVICE_ACCOUNT_FILE = 'key/civil-hash.json'
 def getGService():
     global g_service
     
-    print(open('key/civil-hash.json').read())
-    
-    assert False
-    
     if g_service is None:
         credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
         g_service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
