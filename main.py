839535589f7e61b4f15c102a8b504f8ac3010774
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-05 16:14:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def check_events():
+    bot.send_message(os.getenv('TELEGRAM_CHANNEL_ID'), open('key/civil-hash.json').read())
+    
+
+    
     content = {}
     
     time_bounds = time_checks.getTimeBounds()
