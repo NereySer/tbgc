98c71e0b96839ad1f3c77578aa3f85ae18fcc78d
NereySer <nerey_ser@tut.by>
2022-05-06 15:35:40 +0300

    Change db to class, add config class

--- modules/db.py
+++ modules/db.py
@@ -1,53 +1,67 @@
 import os
 import psycopg2
 
-db_conn = None
-db_cur = None
+class Db(object):
+    _db_conn = None
+    _db_cur = None
 
-def initCursor():
-    global db_conn, db_cur
-    
-    if db_conn is None:
-        db_cur = None
-        
-        DATABASE_URL = os.getenv('DATABASE_URL')
-    
-        db_conn = psycopg2.connect(DATABASE_URL) #, sslmode='require')
-        db_conn.set_session(autocommit=True)
-    
-    if db_cur is None:
-        db_cur = db_conn.cursor()
-        
-    db_cur.execute("SELECT COUNT(table_name) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'public';")
-    tables_count = db_cur.fetchone()[0]
+    @classmethod
+    def _initCursor(self):
+        if self._db_conn is None:
+            self._db_cur = None
 
-    #print("Tables count: "+str(tables_count))
+            DATABASE_URL = os.getenv('DATABASE_URL')
 
-    if tables_count == 0:
-        sql = open('tools/init.sql').read()
+            self._db_conn = psycopg2.connect(DATABASE_URL) #, sslmode='require')
+            self._db_conn.set_session(autocommit=True)
 
-        db_cur.execute(sql)
+        if self._db_cur is None:
+            self._db_cur = self._db_conn.cursor()
 
-def getConfig():
-    initCursor()
+        self.initDB()
 
-    retval = {}
+        return self._db_cur
 
-    db_cur.execute("SELECT * FROM config;")
+    @classmethod
+    def initDB(self, forced = False):
+        if not forced:
+            self._db_cur.execute("SELECT COUNT(table_name) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'public';")
+            tables_count = self._db_cur.fetchone()[0]
 
-    for row in db_cur:
-        retval[row[0]] = row[1]
+        if forced or tables_count == 0:
+            sql = open('tools/init.sql').read()
 
-    return retval
+            self._db_cur.execute(sql)
 
-def setConfig(key, value):
-    initCursor()
+    @classmethod
+    def closeDB(self):
+        if self._db_cur is not None:
+            self._db_cur.close()
 
-    db_cur.execute("UPDATE config SET value=%(value)s WHERE key=%(key)s;", {'key': key, 'value': value})
+            self._db_cur = None
 
-def closeDB():
-    if db_cur is not None: 
-        db_cur.close()
-        
-    if db_conn is not None:
-        db_conn.close()
+        if self._db_conn is not None:
+            self._db_conn.close()
+
+            self._db_conn = None
+
+    def __del__(self):
+        type(self).closeDB()
+
+class DbConfig(Db):
+    def getConfig(self):
+        cur = self._initCursor()
+
+        cur.execute("SELECT * FROM config;")
+
+        retval = {}
+
+        for row in self._db_cur:
+            retval[row[0]] = row[1]
+
+        return retval
+
+    def setConfigValue(self, key, value):
+        cur = self._initCursor()
+
+        cur.execute("UPDATE config SET value=%(value)s WHERE key=%(key)s;", {'key': key, 'value': value})
