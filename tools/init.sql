1c67f706cf042ca70b1f49ba5a63b4dac1278747
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-06 15:49:45 +0300

    Update init.sql

--- tools/init.sql
+++ tools/init.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS config
+DROP TABLE IF EXISTS config;
 CREATE TABLE config (key varchar PRIMARY KEY, value varchar);
 INSERT INTO config (key, value)
   VALUES ('last_send', '0000-00-00T00:00:00')
