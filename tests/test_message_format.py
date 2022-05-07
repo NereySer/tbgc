758c663f6932dff784b5220a8d8f21f4d75ce708
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-07 20:50:58 +0300

    Update test_message_format.py

--- tests/test_message_format.py
+++ tests/test_message_format.py
@@ -3,31 +3,27 @@ import pytest
 from modules import message_format
 from datetime import datetime, timedelta
 
-@pytest.mark.parametrize("events", [
-    ([
-        {
-            'summary': 'test',
-            'start': {
-                'dateTime': '2022-05-03T19:00:00+03:00'
-            }
+def generate_event(hour, text):
+    return {
+        'summary': text,
+        'start': {
+            'dateTime': '2022-05-03T$(hour):00:00+03:00'
         }
-    ]),
+    }
+    
+@pytest.mark.parametrize("events, diff, expected", [
     ([
-        {
-            'summary': 'test',
-            'start': {
-                'dateTime': '2022-05-03T19:00:00+03:00'
-            }
-        }, 
-        {
-            'summary': 'second_test',
-            'start': {
-                'dateTime': '2022-05-03T20:00:00+03:00'
-            }
-        }
-    ])
+        generate_event(19, 'test')
+    ], 0, 'Сегодня, вторник, test в 19:00'),
+    ([
+        generate_event(19, 'test')
+    ], 1, 'Завтра, вторник, test в 19:00'),
+    ([
+        generate_event(19, 'test'),
+        generate_event(20, 'second_test')
+    ], 0, 'Сегодня, вторник
+19:00 - test
+20:00 - second_test')
 ])
-def test_work(events):
-    message_format.telegram(events, 0)
-    message_format.telegram(events, 1)
-    message_format.telegram(events, 2)
+def test_work(events, diff, expected):
+    message_format.telegram(events, diff) == expected
