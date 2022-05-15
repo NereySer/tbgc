ef231f967d9353e23641a22b3423ed4de6b400eb
NereySer <67207071+NereySer@users.noreply.github.com>
2022-05-15 17:42:12 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,11 +9,22 @@ def test_raising():
     response = tester.get('/check_events?key='+os.getenv('CHECK_KEY'))
     assert response.status_code >= 200 and response.status_code <= 299
 
-def test_notifications():
+def test_notifications(monkeypatch):
     tester = main.app.test_client()
     
     response = tester.get('/')
     assert response.status_code >= 200 and response.status_code <= 299
     
     print(response.text)
+
+    def mock_g_cal_get_incomig_events():
+        return []
+    
+    monkeypatch.setattr(main.g_cal, 'get_incomig_events', mock_g_cal_get_incomig_events)
+
+    response = tester.get('/')
+    assert response.status_code >= 200 and response.status_code <= 299
+    
+    print(response.text)
+
     assert false
