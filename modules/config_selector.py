66ba184845253abb3f86f7d48266a6f42ed682de
SokolovS <nerey_ser@tut.by>
2023-08-26 22:11:15 +0300

    fix: Synthax

--- modules/config_selector.py
+++ modules/config_selector.py
@@ -2,8 +2,8 @@ import os
 from . import config, config_redis
 
 def Config():
-    if 'KV_URL' in os.environ and 
-        'KV_REST_API_URL' in os.environ and 
+    if 'KV_URL' in os.environ and \
+        'KV_REST_API_URL' in os.environ and \
         'DEPLOY' in os.environ:
         return config_redis.Config(os.getenv('DEPLOY'))
     elif 'DATABASE_URL' in os.environ:
